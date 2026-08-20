90d5404a72920691ed054be728eed595028854d8
Fernando Melo <fgmelofilho@gmail.com>
2026-08-20 17:21:17 -0400

    second commit

--- src/__init__.py
+++ src/__init__.py
@@ -18,6 +18,8 @@ def image_batch_generator(folder_path, batch_size=32, target_size=(224, 224)):
     image_paths = [p for p in folder.rglob('*') if p.suffix.lower() in ['.jpg', '.jpeg', '.png']]
     
     num_images = len(image_paths)
+
+
     
     while True:
         for i in range(0, num_images, batch_size):
@@ -29,13 +31,17 @@ def image_batch_generator(folder_path, batch_size=32, target_size=(224, 224)):
                 img = cv.imread(str(path))
                 if img is not None:
                     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-                    bilateralFilter = cv.bilateralFilter(gray, 9, 75, 75)
-                    # edges = cv.Canny(bilateralFilter, 100, 200)
+                    blur = cv.blur(gray, 3, 3)
+                    threshold1 = 50
+                    threshold2 = 150
+                    canny = cv.Canny(blur, threshold1, threshold2, 3)
                     batch_images.append(bilateralFilter)
-                    cv.imshow("My image", bilateralFilter)
+                    cv.imshow("My image", canny)
+                    # cv.imshow("No filter", img)
                     cv.waitKey(0)
                     cv.destroyAllWindows()
             yield np.array(batch_images)
 
-data_gen = image_batch_generator("data/SROIE2019/train/img", batch_size=5)
+# data_gen = image_batch_generator("data/SROIE2019/train/img", batch_size=5)
+data_gen = image_batch_generator("data/CORD/train/image", batch_size=5)
 first_batch = next(data_gen)
